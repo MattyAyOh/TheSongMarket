c5fb09b35598c608c4e6d24dcdaa2a8e2aa7fcc6
Matt Ao <mattyayoh@gmail.com>
2015-01-19 18:14:35 -0500

    Update createDB script for price update date saving

--- createDB.py
+++ createDB.py
@@ -6,13 +6,13 @@ def create_database():
     db = sqlite3.connect('records.sqlite')
     db.execute('CREATE TABLE IF NOT EXISTS `ytviewcount` ( \
     `trackid`   INTEGER NOT NULL, \
-    `artistid`  INTEGER NOT NULL, \
+    `artistid`  TEXT NOT NULL, \
     `spotifyuri`    TEXT NOT NULL, \
     `youtubeuri`    TEXT NOT NULL, \
     `viewcount` INTEGER NOT NULL, \
     PRIMARY KEY(trackid))')
     db.execute('CREATE TABLE IF NOT EXISTS `artistaverages` ( \
-    `artistid`  INTEGER NOT NULL, \
+    `artistid`  TEXT NOT NULL, \
     `average` INTEGER NOT NULL, \
     PRIMARY KEY(artistid))')
     db.commit()
@@ -25,6 +25,11 @@ def create_database():
     `artistaverage` INTEGER NOT NULL, \
     `ipoprice` INTEGER NOT NULL, \
     PRIMARY KEY(trackid))')
+    db.commit()
+    db.execute('CREATE TABLE IF NOT EXISTS `priceupdatedates` ( \
+    `updateid`  INTEGER NOT NULL, \
+    `date` TEXT NOT NULL, \
+    PRIMARY KEY(udpateid))')
     db.commit()
     db.close()
 
